42c60f5c4b4e7a9a6ef30ef13979794e63c23931
Arnau González <arnau.gonzalez@brandcrumb.com>
2015-05-30 18:58:57 +0200

    embed youtube videos without js library

--- web/js/video_home.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$('#video_home').youTubeEmbed({
-		video		: 'http://www.youtube.com/watch?v=r0bWxYSfT2M',
-		width		: 758, 		// Height is calculated automatically
-		progressBar	: true		// Hide the progress bar
-	});
-});
