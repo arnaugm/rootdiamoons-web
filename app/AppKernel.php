1b82a3aa4dfe5ab8619f2f628d0d50eeefebeaa7
Arnau González <arnaugm@gmail.com>
2017-05-02 23:24:13 +0200

    wip integration

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
 
             new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
+            new RootDiamoons\BandAccountingBundle\RootDiamoonsBandAccountingBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
